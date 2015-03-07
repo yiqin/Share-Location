bc556b6d3a0560c386d4336b22ceb593f9b2261c
yiqin <yiqin.mems@gmail.com>
2015-03-06 22:30:33 -0500

    Initial Commit

---
+++ TestAnvil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAnvil
+//
+//  Created by Yi Qin on 3/6/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAnvil/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAnvil
+//
+//  Created by Yi Qin on 3/6/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
