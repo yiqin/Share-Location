aadaf2a974ddf623e79acc0b8b3247b4d053c52b
yiqin <yiqin.mems@gmail.com>
2015-03-07 09:15:13 -0500

    TestAnvilLocationManager

---
+++ TestAnvil/TestAnvilLocationManager.swift
@@ -0,0 +1,68 @@
+//
+//  TestAnvilLocationManager.swift
+//  TestAnvil
+//
+//  Created by Yi Qin on 3/7/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class TestAnvilLocationManager: NSObject, CLLocationManagerDelegate{
+    
+    let myLocationManager = CLLocationManager()
+    
+    class var sharedInstance : TestAnvilLocationManager {
+        struct Static {
+            static let instance = TestAnvilLocationManager()
+        }
+        return Static.instance
+    }
+    
+    func startLocationManager() {
+        myLocationManager.delegate = self;
+        myLocationManager.pausesLocationUpdatesAutomatically = false
+        myLocationManager.activityType = CLActivityType.Fitness
+        myLocationManager.distanceFilter = kCLDistanceFilterNone
+        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == CLAuthorizationStatus.NotDetermined {
+            println("send the request.....")
+            myLocationManager.requestAlwaysAuthorization()
+        }
+        
+        myLocationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+            if status == CLAuthorizationStatus.Authorized || status == CLAuthorizationStatus.AuthorizedWhenInUse {
+                manager.startUpdatingLocation()
+                // ...
+            }
+    }
+    
+    //CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var location:CLLocation = locations[locations.count-1] as CLLocation
+        
+        println("locations = \(locations)")
+        
+        // Post HTTTP request....
+        
+        var object = PFObject(className: "test")
+        object["title"] = "this is a test."
+        object.saveInBackgroundWithBlock { (success:Bool!, error:NSError!) -> Void in
+            println("save this is a test.")
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println(error)
+        
+    }
+}
