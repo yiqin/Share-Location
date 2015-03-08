02e14f787bc9f4c4eefce030a061d937e30004f4
yiqin <yiqin.mems@gmail.com>
2015-03-08 06:31:49 -0400

    Update some UI....

--- TestAnvil/InfoView.swift
+++ TestAnvil/InfoView.swift
@@ -15,64 +15,98 @@ class InfoView: UIView {
 	var totalEarningsLabel:UILabel! = nil
 	var enableLocationSwitch:UISwitch! = { var l = UISwitch(frame: CGRectMake(310, 5, 100, 50)); return l}()
 	
-	
-	//Internal properties
-	var rateTitleLabel:UILabel = {  var l = UILabel(frame: CGRectMake(2, 55, 100, 20));
-									l.text = "Rate";
-									l.textColor = UIColor.whiteColor();
-									return l}()
-	
-	var enableTitleLabel:UILabel = {  var l = UILabel(frame: CGRectMake(2, 5, 250, 20));
-		l.text = "Enable Location uploading ";
-		l.textColor = UIColor.whiteColor();
-		return l}()
-	
-	var hitsTitleLabel:UILabel = {	var l = UILabel(frame: CGRectMake(112, 55, 140, 20));
-									l.text = "Lookups Today" ;
-									l.textColor = UIColor.whiteColor();
-									return l}();
-	
-	var earningsTitleLabel:UILabel = {	var l = UILabel(frame: CGRectMake(262, 55, 140, 20));
-										l.text = "Total Earnings" ;
-										l.textColor = UIColor.whiteColor();
-										return l}();
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    private let xPadding : CGFloat = 10
+    
 	 override  init(frame: CGRect) {
 		super.init(frame:frame)
+        
+        
+        var enableTitleLabel:UILabel = {  var l = UILabel(frame: CGRectMake(2, 5, 250, 20));
+            l.text = "Enable Location uploading ";
+            l.textColor = UIColor.whiteColor();
+            l.font = UIFont(name: "OpenSans-Semibold", size: 17)
+            return l}()
+        
+        
+        
+        let labelWidth = (CGRectGetWidth(frame)-2*xPadding)/3
+        
+        var rateTitleLabel:UILabel = {  var l = UILabel(frame: CGRectMake(self.xPadding, 55, labelWidth, 20));
+            l.text = "Rate";
+            l.font = UIFont(name: "OpenSans-Semibold", size: 15)
+            l.textColor = UIColor.whiteColor()
+            l.textAlignment = NSTextAlignment.Center
+            return l}()
+
+        
+        var hitsTitleLabel:UILabel = {	var l = UILabel(frame: CGRectMake(self.xPadding+labelWidth, 55, labelWidth, 20));
+            l.text = "Lookups" ;
+            l.font = UIFont(name: "OpenSans-Semibold", size: 15)
+            l.textAlignment = NSTextAlignment.Center
+            l.textColor = UIColor.whiteColor();
+            return l}();
+        
+        var earningsTitleLabel:UILabel = {	var l = UILabel(frame: CGRectMake(self.xPadding+2*labelWidth, 55, labelWidth, 20));
+            l.text = "Earnings" ;
+            l.font = UIFont(name: "OpenSans-Semibold", size: 15)
+            l.textAlignment = NSTextAlignment.Center
+            l.textColor = UIColor.whiteColor();
+            return l}();
+        
 		addSubview(rateTitleLabel)
 		addSubview(hitsTitleLabel)
 		addSubview(earningsTitleLabel)
 		addSubview(enableTitleLabel)
+        
+        
 		addSubview(enableLocationSwitch)
-		enableLocationSwitch.setOn(true, animated: true)
-		rateLabel = UILabel(frame: CGRectMake(2, 95, 100, 20));
+        enableLocationSwitch.setOn(true, animated: true)
+        
+        
+        
+        
+        let tempRateLabelOffsetY = CGRectGetMaxY(rateTitleLabel.frame)+10
+        
+        let tempViewWidth : CGFloat = 30
+        let tempViewHeigth : CGFloat = 3;
+        
+        let tempOffsetForView = (labelWidth-tempViewWidth)*0.5
+        
+        let tempView1 = UIView(frame: CGRectMake(self.xPadding+tempOffsetForView, CGRectGetMaxY(rateTitleLabel.frame)+3, tempViewWidth, tempViewHeigth))
+        tempView1.backgroundColor = UIColor(fromHexString: "3d5fc4")
+        addSubview(tempView1)
+        
+        let tempView2 = UIView(frame: CGRectMake(self.xPadding+labelWidth+tempOffsetForView, CGRectGetMaxY(rateTitleLabel.frame)+3, tempViewWidth, tempViewHeigth))
+        tempView2.backgroundColor = UIColor(fromHexString: "3d5fc4")
+        addSubview(tempView2)
+        
+        let tempView3 = UIView(frame: CGRectMake(self.xPadding+2*labelWidth+tempOffsetForView, CGRectGetMaxY(rateTitleLabel.frame)+3, tempViewWidth, tempViewHeigth))
+        tempView3.backgroundColor = UIColor(fromHexString: "3d5fc4")
+        addSubview(tempView3)
+        
+        
+		rateLabel = UILabel(frame: CGRectMake(self.xPadding, tempRateLabelOffsetY, labelWidth, 20));
 		rateLabel.text = "6 cents"
+        rateLabel.font = UIFont(name: "Lato-Regular", size: 15)
 		rateLabel.textColor = UIColor.whiteColor()
+        rateLabel.textAlignment = NSTextAlignment.Center
 		addSubview(rateLabel)
 		
-		hitsTodayLabel = UILabel(frame: CGRectMake(112, 95, 140, 20));
+		hitsTodayLabel = UILabel(frame: CGRectMake(self.xPadding+labelWidth, tempRateLabelOffsetY, labelWidth, 20));
 		hitsTodayLabel.text = "11 hits"
+        hitsTodayLabel.font = UIFont(name: "Lato-Regular", size: 15)
 		hitsTodayLabel.textColor = UIColor.whiteColor()
-
+        hitsTodayLabel.textAlignment = NSTextAlignment.Center
 		addSubview(hitsTodayLabel)
 	
-		totalEarningsLabel = UILabel(frame: CGRectMake(262, 95, 140, 20));
+		totalEarningsLabel = UILabel(frame: CGRectMake(self.xPadding+2*labelWidth, tempRateLabelOffsetY, labelWidth, 20));
 		totalEarningsLabel.text = "$48.20"
+        totalEarningsLabel.font = UIFont(name: "Lato-Regular", size: 15)
 		totalEarningsLabel.textColor = UIColor.whiteColor()
-
+        totalEarningsLabel.textAlignment = NSTextAlignment.Center
 		addSubview(totalEarningsLabel)
 		
 		backgroundColor = UIColor.blackColor()
-		
-		
 	}
 
 	 required init(coder aDecoder: NSCoder) {
